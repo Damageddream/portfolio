c7df037de168531e8cb2aa3553e6e79a59e8acc7
Damageddream <marcan.grab@gmail.com>
2023-06-28 19:20:05 +0200

    added elements to homepage, added button component

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -1,6 +1,20 @@
-const Button: React.FC = () => {
-    return <button className="btn">
-    </button>
+import { ReactNode, MouseEventHandler, useState } from "react";
+import '../../assets/styles/button.css'
+
+interface ButtonProps {
+  children: ReactNode;
+  onClick?: MouseEventHandler;
+  variant: "primary" | "secondary";
 }
+
+const Button: React.FC<ButtonProps> = ({ children, onClick, variant }) => {
+    const [buttonClass, setButtonClass] = useState(variant)
+
+  return (
+    <button className={buttonClass} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
 
 export default Button;
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,7 +1,38 @@
-const HomePage: React.FC = () => {
-    return <>
-    My homepages
-    </>
-}
+import dribble from "../../assets/images/dribble.png";
+import linkedin from "../../assets/images/linkedin.png";
+import github from "../../assets/images/github.png";
+import figma from "../../assets/images/figma.png";
+import profilPic from "../../assets/images/profilePict.png";
+import Button from "../../components/UI/Button";
+import { useNavigate } from "react-router-dom";
 
-export default HomePage 
+const HomePage: React.FC = () => {
+  const navigate = useNavigate();
+  return (
+    <main className="home">
+      <div className="homeMain">
+        <div className="icons">
+          <img src={github} alt="icon of github" />
+          <img src={linkedin} alt="icon of linkedin" />
+          <img src={dribble} alt="icon of dribble" />
+          <img src={figma} alt="icon of figma" />
+        </div>
+        <header className="homeTitle">
+          <p className="hello">Hello</p>
+          <h2 className="name">
+            <span className="im">I'm </span>Marcin Grabowski
+          </h2>
+        </header>
+        <div className="homeInfo">
+          A full stack developer based in Poland
+        </div>
+        <Button onClick={() => navigate("/")} variant="primary">
+          My portfolio
+        </Button>
+      </div>
+      <img src={profilPic} alt="A headshot photograph of Marcin" />
+    </main>
+  );
+};
+
+export default HomePage;
